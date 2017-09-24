273fac9f40f4956329177e4eefba7bbd470b2401
Achim Abeling <achimabeling@web.de>
2017-09-24 15:42:46 +0200

    added the face detection demo from http://docs.opencv.org/3.0-beta/doc/tutorials/introduction/desktop_java/java_dev_intro.html

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-http://docs.opencv.org/3.3.0/d7/d9f/tutorial_linux_install.html
+My first steps using opencv.
 
-     $ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DBUILD_SHARED_LIBS= -DBUILD_EXAMPLES= -D BUILD_TESTS= -DBUILD_PERF_TESTS=  ~/Projects/github/opencv
-     $ make -j4
-     $ sudo make install
-
-uninstall: nochmal cmake und dann make uninstall ?
-
+opencv has been installed on my ubuntu 16.04. with
 
      $ sudo apt install libcv2.4
      $ sudo apt install libopencv2.4-java
+
+Then it just ran fine with
+
+     $ ./gradlew bootRun
--- src/main/java/de/banapple/moelmo/MoelmoApplication.java
+++ src/main/java/de/banapple/moelmo/MoelmoApplication.java
@@ -1,8 +1,13 @@
 package de.banapple.moelmo;
 
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.highgui.Highgui;
+import org.opencv.objdetect.CascadeClassifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,19 +17,58 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class MoelmoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoelmoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MoelmoApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner runner(ApplicationContext context) {
+    @Bean
+    public CommandLineRunner runner(ApplicationContext context) {
 
-		return args -> {
+        return args -> {
 
-			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-			Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-			System.out.println("mat = " + mat.dump());
-			
-		};
-	}
+            System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+            System.out.println("Welcome to OpenCV " + Core.VERSION);
+
+            detectFace();
+
+        };
+    }
+
+    /**
+     * Takes a picture with some faces and detects them.
+     * The result is written to a file and faces are marked
+     * with rectangles.
+     */
+    public void detectFace() {
+
+        System.out.println("\nRunning DetectFaceDemo");
+
+        // Create a face detector from the cascade file in the resources
+        // directory.
+        CascadeClassifier faceDetector = new CascadeClassifier(
+                getClass().getResource("/lbpcascade_frontalface.xml").getPath());
+        Mat image = Highgui.imread(getClass().getResource("/beatles.jpg").getPath());
+
+        // Detect faces in the image.
+        // MatOfRect is a special container class for Rect.
+        MatOfRect faceDetections = new MatOfRect();
+        faceDetector.detectMultiScale(image, faceDetections);
+
+        System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
+
+        // Draw a bounding box around each face.
+        for (Rect rect : faceDetections.toArray()) {
+            System.out.println(rect.toString());
+            Core.rectangle(image,
+                    new Point(rect.x, rect.y),
+                    new Point(rect.x + rect.width, rect.y + rect.height),
+                    new Scalar(0, 255, 0));
+        }
+
+        // Save the visualized detection.
+        String filename = "/tmp/faceDetection.png";
+        System.out.println(String.format("Writing %s", filename));
+        Highgui.imwrite(filename, image);
+    }
 }
